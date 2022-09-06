d0970f9983a7b2b2bf4ce8521d8b8a68282bcb4e
Stanislaw <stas.yarets234@gmail.com>
2022-09-06 12:09:08 +0300

    del hello

--- src/com/yarets/droidbattle/Main.java
+++ src/com/yarets/droidbattle/Main.java
@@ -4,7 +4,6 @@ import com.yarets.droidbattle.TypesofDroids.DoubleProtectionDroid;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
         Battle initBattle = new Battle();
     }
 }
